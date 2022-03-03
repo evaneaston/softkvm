d28e34d9b7fe54ba0d883e47083fe0827706daf9
Evan Easton <evan@eeaston.com>
2022-03-03 11:54:19 -0600

    Add config.  Cleanup switch.sh for readability.

---
+++ README.md
@@ -0,0 +1,8 @@
+# softkvm
+
+## Background
+
+* Used [marcelhoffs/input-switcher](https://github.com/marcelhoffs/input-switcher) as basis for my keyboard/mouse switching scripts.
+* [todbot/hidapitester](https://github.com/todbot/hidapitester) is tool for sending messages to mouse and keyboard drivers
+* nirsoft's [ControlMyMonitor](https://www.nirsoft.net/utils/control_my_monitor.html) is used to send DDC/CI messages to my display
+  * specifically [How to set monitor input source](https://www.nirsoft.net/articles/set_monitor_input_source_command_line.html)
--- switch.sh
+++ switch.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 RCVR_VID=046D
 RCVR_PID=C52B
@@ -12,6 +13,10 @@ MOUSE_NUMBER=0x02
 KEYBOARD_CHANNEL_PREFIX=0x09,0x1e
 MOUSE_CHANNEL_PREFIX=0x0a,0x1b
 
+usage() {
+    echo "Usage $0 {-f} {-v 4|6} hostname ipaddress\n\n  -f     FQDN only.  Sets registerNonQualified=false on the host record.  Defaults to true.\n  -v     4 or 6.  Defaults to 4.  Ignored by UDM firmware < 1.9.\n"
+}
+
 sendHidCommand() {
     deviceNumber=$1
     channelPrefix=$2
@@ -24,32 +29,23 @@ sendHidCommand() {
         --length 7 \
         --send-output 0x10,${deviceNumber},${channelPrefix},${receiverChannel},0x00,0x00
 }
+
+switchKeyboardTo() {
+   channel=$1
+   sendHidCommand ${KEYBOARD_NUMBER} ${KEYBOARD_CHANNEL_PREFIX} ${channel}
+}
+
+switchMouseTo() {
+   channel=$1
+   sendHidCommand ${MOUSE_NUMBER}    ${MOUSE_CHANNEL_PREFIX}    ${channel}
+}
+
+
 switchTo() {
     channel=$1
-    sendHidCommand ${KEYBOARD_NUMBER} ${KEYBOARD_CHANNEL_PREFIX} ${channel}
-    sendHidCommand ${MOUSE_NUMBER}    ${MOUSE_CHANNEL_PREFIX}    ${channel}
+    switchKeyboardTo ${channel}
+    switchMouseTo ${channel}    
 }
 
 switchTo ${WORK_CHANNEL}
-
-# # Switch MX Keys to other device
-# ./hidapitester.exe \
-#      --vidpid ${RCVR_VID}:${RCVR_PID} \
-#     --usage 0x0001 \
-#     --usagePage 0xFF00 \
-#     --open \
-#     --length 7 \
-#     --send-output 0x10,${KEYBOARD_NUMBER},0x09,0x1e,${WORK},0x00,0x00
-
-
-# # Switch MX Mouse to other device
-# ./hidapitester.exe \
-#     --vidpid ${RCVR_VID}:${RCVR_PID} \
-#     --usage 0x0001 \
-#     --usagePage 0xFF00 \
-#     --open \
-#     --length 7 \
-#     --send-output 0x10,0x02,0x0a,0x1b,0x01,0x00,0x00
-
-# exit 0
 
